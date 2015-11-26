c0c18e2edcc7f891a2851208f3f82ed37b6911b0
Tobias Abarbanell <tobias@abarbanell.de>
2015-11-26 20:51:42 +0100

    print IP sketch working

---
+++ sketch_wifi_print_IP/README.md
@@ -0,0 +1,48 @@
+# sketch_wifi_print_IP
+
+This is an example from the [wino-board website](http://www.wino-board.com) how to get the IP from your wino-board.
+
+It uses the ESP8266 AT-commands becauset he wifi library is not yet stable.
+
+
+WIFI passwords will be injected via a local file config.h - but at this
+time they are not yet used.
+
+But anyway here is how to use this sketch: 
+
+# Usage
+
+First you can 
+
+```
+$ git clone git@github.com:abarbanell/arduino-wino.git
+$ cd arduino-wino/sketch_wifi_print_IP
+```
+
+However, before you compile this sketch you need to create a file
+"config.h" with your own config values like this: 
+
+```
+/*
+ * config variables - this file should not be in source control as it 
+ * will show your live credentials.
+ */
+
+#ifndef WIFI_SSID
+#define WIFI_SSID "MY-SSID"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD "MY-WIFI-PASSWORD"
+#endif
+```
+
+Of course you want to have your own values in here. (Note again: these
+values are not yet used, so it actually does not matter which values
+you use...)
+
+Then you can compile and upload the sketch to your arduino as usual and
+
+you should see one of the config values printed over the serial monitor
+whenever you send any byte down to your arduino.
+
--- sketch_wifi_print_IP/sketch_wifi_print_IP.ino
+++ sketch_wifi_print_IP/sketch_wifi_print_IP.ino
@@ -1,3 +1,6 @@
+
+#include "config.h" 
+
 String answer;
 String ip = "";
 void setup() {
@@ -9,54 +12,56 @@ void setup() {
   Serial.begin(115200); //Opens serial connection to WIFI module
   SerialUSB.begin(115200); //Opens USB-Serial connection for terminal
  
+  delay(5000);
+  SerialUSB.write("Serial interface is ready\r\n");
+
  
- //Preparing Wifi-module
- digitalWrite(WIFI_EN_PIN, LOW); //turns WIFI module off
- digitalWrite(13, HIGH); //set module to normal mode
- delay(100);              // wait a short time
- digitalWrite(WIFI_EN_PIN, HIGH); //turns WIFI module on
+  //Preparing Wifi-module
+  digitalWrite(WIFI_EN_PIN, LOW); //turns WIFI module off
+  digitalWrite(13, HIGH); //set module to normal mode
+  delay(100);              // wait a short time
+  digitalWrite(WIFI_EN_PIN, HIGH); //turns WIFI module on
  
  
-    //wait until the module is ready
-    while (!Serial.find("ready\r\n")) {}
-    SerialUSB.write("Module is ready\r\n");
+  //wait until the module is ready
+  while (!Serial.find("ready\r\n")) {}
+  SerialUSB.write("Module is ready\r\n");
   
-    //Set module to Station mode and wait for reply
-    Serial.println("AT+CWMODE=1");
-    while (!Serial.find("OK\r\n")) {}
-    SerialUSB.println("Module changing to Station mode");
-    //delay(100);
+  //Set module to Station mode and wait for reply
+  Serial.println("AT+CWMODE=1");
+  while (!Serial.find("OK\r\n")) {}
+  SerialUSB.println("Module changing to Station mode");
+  delay(100);
  
-    //Waiting for IP-Address
-    while (!Serial.find("WIFI GOT IP")) {SerialUSB.println("Waiting for IP-Address"); delay(200);}
-    SerialUSB.println("got IP-Address");
+  //Waiting for IP-Address
+  while (!Serial.find("WIFI GOT IP")) {
+    SerialUSB.println("Waiting for IP-Address"); 
+    delay(200);
+  }
+  SerialUSB.println("got IP-Address");
  
-    //Reading IP-Address
-    Serial.println("AT+CIFSR");
+  //Reading IP-Address
+  Serial.println("AT+CIFSR");
     
   
 }
  
 void loop() {
- 
-while (Serial.available() > 0)
-    {
-        char received = Serial.read();
-        answer.concat(received);
-        if (answer.indexOf("\r\n") > -1 ){
- 
-          if (answer.indexOf("STAIP") > -1) {
-            int starti = answer.indexOf('IP,"') + 1;
-            ip = answer.substring(starti,answer.length() - 3);
-          } 
-         answer=""; 
-        }
-        // Process message when new line character is recieved
-       
+  while (Serial.available() > 0) {
+    char received = Serial.read();
+    answer.concat(received);
+    if (answer.indexOf("\r\n") > -1 ){
+      if (answer.indexOf("STAIP") > -1) {
+        int starti = answer.indexOf('IP,"') + 1;
+        ip = answer.substring(starti,answer.length() - 3);
+      } 
+      answer=""; 
     }
- 
-if (ip != "") {SerialUSB.println(ip); delay(1000);}
- 
- 
+    // Process message when new line character is received
+  }
+  if (ip != "") {
+    SerialUSB.println(ip); 
+    delay(1000);
+  }
 }
 
