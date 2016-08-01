83506285075f653fb3d1c06a206604bfb874b56e
Tobias Abarbanell <tobias@abarbanell.de>
2016-08-01 17:07:47 +0200

    cleanup of debug messages

--- sketch_soil_wifi/sketch_soil_wifi.ino
+++ sketch_soil_wifi/sketch_soil_wifi.ino
@@ -27,8 +27,6 @@
         }\
     } while(0)
 
-
-
 int loopcnt = 0;
 int errcnt = 0;
 
@@ -58,36 +56,31 @@ void wifi_setup(void) {
   SerialUSB.println(wifi.info());
  
   delay(5000);
-
   wifi.setMode(0);
 
-  logDebug(wifi.list());
   while (! wifi.join(WIFI_SSID, WIFI_PASSWORD)) {
     logDebug('wifi_setup(): connecting to WIFI...');
     delay(2000);
   }
-  SerialUSB.print("wifi_setuip(): connected to WIFI. IP: ");
-  SerialUSB.println(wifi.getip());
-  SerialUSB.print("wifi_setup(): connected to WIFI. MAC: ");
-  SerialUSB.println(wifi.getmac());
-  SerialUSB.print("wifi_setup(): IP from my own getip(): ");
-  SerialUSB.println( getIP(true)); // short version of IP, cached
-  SerialUSB.print("wifi_setup(): gethostname(): ");
-  SerialUSB.println(gethostname(true)); // short version of hostname, cached
+  logDebug("connected to WIFI. IP: ");
+  logDebug(wifi.getip());
+  logDebug("connected to WIFI. MAC: ");
+  logDebug(wifi.getmac());
+  logDebug("gethostname(): ");
+  logDebug(gethostname(true)); // short version of hostname, cached
   
 }
 
 void sensor_setup(void) {
   pinMode(powerPin, OUTPUT);  // Sensor Power
   pinMode(ledPin, OUTPUT);  // LED when measuring
-  SerialUSB.println("sensor_setup(): done"); 
+  logDebug("sensor_setup(): done"); 
 }
   
 
 
 void loop() {
-  SerialUSB.print("loop():  loops (minutes) since restart: ");
-  SerialUSB.println(loopcnt);
+  logDebug("loops (minutes) since restart: " + loopcnt);
     
   loopcnt++;
   if ((loopcnt % 1) == 0) lg_heartbeat(); // every 1 loops
@@ -122,20 +115,20 @@ String gethostname(bool cache) {
 
  void lg_heartbeat(void) {
   String payload = "{ \"host\": \"" + gethostname(true) + "\", \"loopcount\": " + loopcnt + ", \"errcount\": " + errcnt + " }"; 
-  String request = "POST /api/collections/heartbeat?user_key=" + String(LG_API_KEY) + " HTTP/1.1\r\n"
-    + "Host: " + LG_HOST + "\r\n"
-    + "User-Agent: arduino-wino/1.0 \r\n"
-    + "Content-Type: application/json \r\n"
-    + "Connection: close \r\n"
-    + "Content-Length: " + payload.length() + "\r\n"
-    + "\r\n"
-    + payload + "\r\n";
-  http_post(request);
+  String url = "/api/collections/heartbeat?user_key=" + String(LG_API_KEY);
+  http_post(url, payload);
  }
  
  void lg_sensor(int val) {
   String payload = "{ \"host\": \"" + gethostname(true) + "\", \"soil\": " + val + " }"; 
-  String request = "POST /api/collections/sensor?user_key=" + String(LG_API_KEY) + " HTTP/1.1\r\n"
+  String url = "/api/collections/sensor?user_key=" + String(LG_API_KEY);
+  http_post(url, payload);
+ }
+
+ void http_post(String url, String payload) {
+  uint8_t buffer[1024] = {0};
+
+  String request = "POST " + url + " HTTP/1.1\r\n"
     + "Host: " + LG_HOST + "\r\n"
     + "User-Agent: arduino-wino/1.0 \r\n"
     + "Content-Type: application/json \r\n"
@@ -143,22 +136,6 @@ String gethostname(bool cache) {
     + "Content-Length: " + payload.length() + "\r\n"
     + "\r\n"
     + payload + "\r\n";
-  http_post(request);
- }
-
- void http_post(String url) {
-  uint8_t buffer[1024] = {0};
-
-  SerialUSB.print("host: ");
-  SerialUSB.print(LG_HOST);
-  SerialUSB.print(" - port: ");
-  SerialUSB.println(LG_PORT);
-  SerialUSB.println("=== HTTP request start ===");
-  SerialUSB.println(url);
- 
-  SerialUSB.println("=== HTTP request end ===");
-  SerialUSB.print("url.length(): ");
-  SerialUSB.println(url.length());
   
   if (wifi.connect("TCP", LG_HOST, LG_PORT)) {
     logDebug("tcp connection created");
@@ -167,18 +144,13 @@ String gethostname(bool cache) {
   }
 
   
-  if (wifi.writeln(url)) {
+  if (wifi.writeln(request)) {
     logDebug("request sent with wifi.writeln(String), lrc = true");  
   } else {
     logDebug("request sent with wifi.writeln(String), lrc = false");      
   }
   uint32_t len = wifi.read(buffer, sizeof(buffer), 10000);
   if (len > 0) {
-    SerialUSB.print("http_post(): Received: [");
-    for (uint32_t i=0; i<len; i++) {
-      SerialUSB.print((char)buffer[i]);  
-    }
-    SerialUSB.println("]");
     logDebug((char *)buffer);
   } else {
     logDebug("<empty response>");
@@ -199,12 +171,10 @@ void blinkLed(int ms) {
 int soil_measure(void) {
   int val;
 
-  digitalWrite(ledPin, HIGH);
   digitalWrite(powerPin, HIGH);
-  delay(2000);
+  delay(1000);
   val = analogRead(A2);
   digitalWrite(powerPin, LOW);
-  digitalWrite(ledPin, LOW);
   return val;
 }
 
